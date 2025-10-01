a19d80b99f1dc1185d7ada4664171684d8830780
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-10-01 18:13:06 -0300

    feat: autenticação e login

--- frontend-gestao-marketplace/src/app/services/user-auth.ts
+++ frontend-gestao-marketplace/src/app/services/user-auth.ts
@@ -5,6 +5,10 @@ import { Injectable } from '@angular/core';
 })
 export class UserAuthService {
   getUserToken() {
-    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIsImVtYWlsIjoibWF0dGhldXNAaG90bWFpbC5jb20iLCJpYXQiOjE3NTkyNjMwNTgsImV4cCI6MTc1OTM0OTQ1OH0.YfJooC5XktCHAJ6kI-myOtaGICV8grEp_jO4VtRgAVE';
+    return localStorage.getItem('auth-token');
+  }
+
+  setUserToken(token: string) {
+    localStorage.setItem('auth-token', token);
   }
 }
