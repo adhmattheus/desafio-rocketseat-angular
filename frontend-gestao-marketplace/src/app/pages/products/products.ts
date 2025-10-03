30567b5041df81af991123ca0d449184c7e5dbac
Mattheus Adhonnay <mamenezes@viceri.com.br>
2025-10-03 16:27:31 -0300

    feat: filtro de produtos

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -2,23 +2,51 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductsService } from '../../services/products';
 import { take } from 'rxjs';
 import { IProductResponse } from '../../interfaces/product-response';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css',
 })
 export class Products implements OnInit {
   products: IProductResponse[] = [];
-  
+  filteredProducts: IProductResponse[] = [];
+
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl(''),
+  });
+
   private readonly _productsService = inject(ProductsService);
 
   ngOnInit() {
-    this._productsService.getProducts().pipe(take(1)).subscribe({
-      next:(response) => {
-        this.products = response.data;
-      },
-    });
+    this._productsService
+      .getProducts()
+      .pipe(take(1))
+      .subscribe({
+        next: (response) => {
+          this.products = response.data;
+          this.filteredProducts = response.data;
+        },
+      });
+  }
+
+  filterProducts() {
+    const title = this.filterForm.get('title')?.value?.toLowerCase();
+    const status = this.filterForm.get('status')?.value?.toLowerCase();
+
+    this.filteredProducts = this.products.filter(
+      (product) =>
+        (!title || product.title.toLowerCase().includes(title)) &&
+        (!status || product.status.toLowerCase().includes(status))
+    );
+  }
+
+  clearFilter() {
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('');
+    this.filteredProducts = this.products;
   }
 }
